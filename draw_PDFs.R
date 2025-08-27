c8b339316c142d6087c3398cc6854859e003fc04
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2025-08-27 20:28:32 +0200

    design finalized and sent out to print

--- draw_PDFs.R
+++ draw_PDFs.R
@@ -20,6 +20,8 @@ dial_pixel_protrudes_max_mm <- (a_mm / pixel_cnt) * .7
 dial_opening_depth <- 2 * sqrt((r_mm + dial_pixel_protrudes_max_mm)^2 - h_mm^2) + 1  #one additional mm just to be on a safe side
 dial_opening_width <- 46 # room for 3 sets of four dials (and one separator) 3 mm each (3*5*3) plus one additional mm just to be on a safe side 
 
+movement_shaft_mm <- 4
+
 shaft_r_mm <- 2.1
 shaft_middle_below_lid <- (h_mm+3) - lid_height_in_feathers*feather_width_mm
 
@@ -95,7 +97,7 @@ draw_three_separators <- function(x, y) {
   lines(c(x + dial_opening_depth + 6, x + dial_opening_depth + 6), c(y + 3, y + 12))
 }
 
-draw_dial <- function(x, y, r, inner_r, pixel_vector=rep(F, pixel_cnt * 10), control=FALSE) {
+draw_dial <- function(x, y, r, inner_r, draw_small_rod_sockets, pixel_vector=rep(F, pixel_cnt * 10), control=FALSE) {
   if (length(pixel_vector) != pixel_cnt * 10)
     stop("Pixel vector must have 10 times pixel_cnt positions")
   
@@ -111,7 +113,8 @@ draw_dial <- function(x, y, r, inner_r, pixel_vector=rep(F, pixel_cnt * 10), con
   current_point <- c(x + cos(angle) * r, y + sin(angle) * r)
   for (side in 1:10) {
     angle = side * (2*pi / 10)
-    draw_arc(x + cos(angle+2*pi/20) * r*4/5, y + sin(angle+2*pi/20) * r*4/5, rod_r_mm, 0, 2*pi, n=256)
+    if (draw_small_rod_sockets)
+      draw_arc(x + cos(angle+2*pi/20) * r*4/5, y + sin(angle+2*pi/20) * r*4/5, rod_r_mm, 0, 2*pi, n=256)
     next_point <- c(x + cos(angle) * r, y + sin(angle) * r)
     #lines(c(current_point[1], next_point[1]), c(current_point[2], next_point[2]))
     #instead of plotting a single line, this line will be divided into 7 regions and the regions will be plotted separately
@@ -292,6 +295,93 @@ draw_alt_side_wall <- function(width_segment_count, height_segment_count, shift,
   
 } 
 
+
+
+
+draw_alt2_side_wall <- function(width_segment_count, height_segment_count, shift, protrude = TRUE) {
+  if (protrude) {
+    current_pos <- c(0, arbitrary_thickness_mm + shaft_middle_below_lid + shaft_r_mm)
+    tmp_end <- c(width_segment_count * feather_width_mm - shift,  current_pos[2]) - c(movement_shaft_mm/2 + shaft_r_mm + arbitrary_thickness_mm, 0)
+  } else {
+    current_pos <- c(0, 0)
+    tmp_end <- c(shift,  current_pos[2]) - c(movement_shaft_mm/2 + shaft_r_mm + arbitrary_thickness_mm, 0)
+  }
+  
+  lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+  
+  if (protrude) {
+    #line down
+    current_pos <- tmp_end
+    tmp_end[2] <- tmp_end[2] - shaft_middle_below_lid
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+    #first arc
+    draw_arc(tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm, tmp_end[2], shaft_r_mm + arbitrary_thickness_mm, pi, pi*3/2)
+    # and the arc of the opening, centered in the same point, but with a smaller radius
+    draw_arc(tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm, tmp_end[2], shaft_r_mm, pi*1/2, pi*3/2)
+    # and the two inner connectors
+    lines(c(tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm, tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm + movement_shaft_mm), c(tmp_end[2] - shaft_r_mm, tmp_end[2] - shaft_r_mm))
+    lines(c(tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm, tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm + movement_shaft_mm), c(tmp_end[2] + shaft_r_mm, tmp_end[2] + shaft_r_mm))
+    
+    
+    #line to the side
+    current_pos <- tmp_end + c(arbitrary_thickness_mm + shaft_r_mm, -arbitrary_thickness_mm - shaft_r_mm)
+    tmp_end <- current_pos + c(movement_shaft_mm, 0)
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+    #second arc
+    draw_arc(tmp_end[1], tmp_end[2] + arbitrary_thickness_mm + shaft_r_mm, shaft_r_mm + arbitrary_thickness_mm, pi*3/2, 2 * pi)
+    # and the arc of the inner opening, centered in the same point, but with a smaller radius
+    draw_arc(tmp_end[1], tmp_end[2] + arbitrary_thickness_mm + shaft_r_mm, shaft_r_mm, -pi*1/2, pi*1/2)
+    
+    
+    #line up
+    current_pos <- tmp_end + c(arbitrary_thickness_mm + shaft_r_mm, arbitrary_thickness_mm + shaft_r_mm)
+    tmp_end <- current_pos + c(0, shaft_middle_below_lid)
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+    
+    current_pos <- c(width_segment_count * feather_width_mm - plywood_thickness_mm, arbitrary_thickness_mm + shaft_middle_below_lid + shaft_r_mm)
+    
+    
+    
+  } else {
+    #line up
+    current_pos <- tmp_end
+    tmp_end[2] <- tmp_end[2] + shaft_middle_below_lid
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+    #first arc
+    draw_arc(tmp_end[1] + arbitrary_thickness_mm + shaft_r_mm, tmp_end[2], shaft_r_mm + arbitrary_thickness_mm, pi, pi*1/2)
+    #line to the side
+    current_pos <- tmp_end + c(arbitrary_thickness_mm + shaft_r_mm, arbitrary_thickness_mm + shaft_r_mm)
+    tmp_end <- current_pos + c(movement_shaft_mm, 0)
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+    #second arc
+    draw_arc(tmp_end[1], tmp_end[2] - arbitrary_thickness_mm - shaft_r_mm, shaft_r_mm + arbitrary_thickness_mm, pi*1/2, 0)
+    #line down
+    current_pos <- tmp_end + c(arbitrary_thickness_mm + shaft_r_mm, -arbitrary_thickness_mm - shaft_r_mm)
+    tmp_end <- current_pos + c(0, -shaft_middle_below_lid)
+    lines(c(current_pos[1], tmp_end[1]), c(current_pos[2], tmp_end[2]))
+ 
+    current_pos <- c(width_segment_count * feather_width_mm - plywood_thickness_mm, 0)
+  }
+  
+  
+  lines(c(tmp_end[1], current_pos[1]), c(tmp_end[2], current_pos[2]))
+  
+  #and a final go up or down as if it were a one large feather
+  coordinate <- 1
+  direction <- 1
+  increase_on_first_feather_side <- TRUE
+  new_pos <- current_pos
+  new_pos[3-coordinate] <- current_pos[3-coordinate] + (-1)^(increase_on_first_feather_side + direction) * plywood_thickness_mm
+  lines(c(current_pos[1], new_pos[1]), c(current_pos[2], new_pos[2]))
+  current_pos <- new_pos
+  
+  current_pos <- draw_feathered_line(height_segment_count,2, current_pos, increase_on_first_feather_side=TRUE, increase_on_feather_width=TRUE, smaller_first=TRUE, smaller_last=FALSE)
+  current_pos <- draw_feathered_line(width_segment_count, 1, current_pos, increase_on_first_feather_side=FALSE, increase_on_feather_width=FALSE, smaller_first=FALSE, smaller_last=TRUE)
+  current_pos <- draw_feathered_line(height_segment_count,2, current_pos, increase_on_first_feather_side=FALSE, increase_on_feather_width=FALSE, smaller_first=TRUE, smaller_last=FALSE)
+  
+  
+} 
+
 draw_inner_back<- function(width_segment_count, height_segment_count) {
   current_pos <- c(plywood_thickness_mm,0)
   current_pos <- draw_straight_line(width_segment_count, 1, current_pos, increase_on_feather_width=TRUE, smaller_first=TRUE, smaller_last=TRUE, skip_feather=TRUE)
@@ -299,7 +389,7 @@ draw_inner_back<- function(width_segment_count, height_segment_count) {
   current_pos <- draw_straight_line(width_segment_count, 1, current_pos, increase_on_feather_width=FALSE, smaller_first=TRUE, smaller_last=TRUE, skip_feather=TRUE)
   current_pos <- draw_straight_line(height_segment_count,2, current_pos, increase_on_feather_width=FALSE, smaller_first=FALSE, smaller_last=TRUE, skip_feather=TRUE)
   
-} 
+}
 
 draw_L <- function(x0, y0, L = c(3, 6, 4, 3, 7, 9)) {
   stopifnot(length(L) == 6)
@@ -323,8 +413,8 @@ draw_L <- function(x0, y0, L = c(3, 6, 4, 3, 7, 9)) {
   
 }
 
-draw_loop_lines_exact <- function(x0, y0,
-                                  len_left = 8, down1 = 3, diag45 = 5, down2 = 5) {
+draw_small_support <- function(x0, y0,
+                                  len_left = 6, down1 = 3, diag45 = 3*sqrt(2), down2 = 6) {
   # Start
   S <- c(x0, y0)
   # 8 mm left
@@ -379,7 +469,7 @@ draw_three_sheets <- function(x0, y0) {
   draw_rectangle(x0, y0 + 27,42,  5)   # 42×5  just below
 }
 
-draw_support <- function(x, y) {
+draw_shaft_support <- function(x, y) {
   draw_narrowing_rectangle(x, y, dial_opening_depth, 1.5 * h_mm)
   draw_arc(x+dial_opening_depth/2, y+h_mm, shaft_r_mm, 0, 2*pi, n=256)
 }
@@ -417,6 +507,15 @@ open.pdf("design_PDFs/base_front_or_back.pdf", case_width_in_feathers*feather_wi
 draw_side_wall(case_width_in_feathers, base_height_in_feathers)
 close.pdf()
 
+open.pdf("design_PDFs/base_back.pdf", case_width_in_feathers*feather_width_mm, base_height_in_feathers*feather_width_mm, 10)
+draw_side_wall(case_width_in_feathers, base_height_in_feathers)
+close.pdf()
+
+open.pdf("design_PDFs/base_front.pdf", case_width_in_feathers*feather_width_mm, base_height_in_feathers*feather_width_mm, 10)
+draw_alt2_side_wall(case_width_in_feathers, base_height_in_feathers, 21, protrude=FALSE)
+close.pdf()
+
+
 open.pdf("design_PDFs/base_inner_back.pdf", case_width_in_feathers*feather_width_mm, base_height_in_feathers*feather_width_mm, 10)
 draw_inner_back(case_width_in_feathers, base_height_in_feathers)
 close.pdf()
@@ -433,9 +532,14 @@ open.pdf("design_PDFs/lid_side_B.pdf", case_depth_in_feathers*feather_width_mm, 
 draw_alt_side_wall(case_depth_in_feathers, lid_height_in_feathers, 21+dial_opening_depth/2, protrude = FALSE)
 close.pdf()
 
-open.pdf("design_PDFs/lid_front_or_back.pdf", case_width_in_feathers*feather_width_mm, lid_height_in_feathers*feather_width_mm, 10)
+open.pdf("design_PDFs/lid_back.pdf", case_width_in_feathers*feather_width_mm, lid_height_in_feathers*feather_width_mm, 10)
 draw_side_wall(case_width_in_feathers, lid_height_in_feathers)
 close.pdf()
+
+open.pdf("design_PDFs/lid_front.pdf", case_width_in_feathers*feather_width_mm, lid_height_in_feathers*feather_width_mm+20, 10)
+draw_alt2_side_wall(case_width_in_feathers, lid_height_in_feathers, 21)
+close.pdf()
+
 
 open.pdf("design_PDFs/lid_inner_back.pdf", case_width_in_feathers*feather_width_mm, lid_height_in_feathers*feather_width_mm, 10)
 draw_inner_back(case_width_in_feathers, lid_height_in_feathers)
@@ -446,24 +550,25 @@ draw_top_or_bottom(case_width_in_feathers, case_depth_in_feathers, dial_opening_
 close.pdf()
 
 inner_r<-c(rep(shaft_r_mm, 2), shaft_r_mm + 14)
+draw_small_rod_sockets<-c(F, T, T)
 
 for (i in 1:3) {
   open.pdf(paste0("design_PDFs/dial", i, ".pdf"), r_mm*6+16, r_mm*2+5, 10)
-  draw_dial(r_mm+5, r_mm+5, r_mm, inner_r[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
-  draw_dial(3*r_mm+10, r_mm+5, r_mm, inner_r[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
-  draw_dial(5*r_mm+15, r_mm+5, r_mm, inner_r[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
+  draw_dial(r_mm+5, r_mm+5, r_mm, inner_r[i], draw_small_rod_sockets[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
+  draw_dial(3*r_mm+10, r_mm+5, r_mm, inner_r[i], draw_small_rod_sockets[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
+  draw_dial(5*r_mm+15, r_mm+5, r_mm, inner_r[i], draw_small_rod_sockets[i], pixel_vector = unlist(lapply(digits, function(m) m[,i])))
   close.pdf()
 }
 
 open.pdf("design_PDFs/dial_control.pdf", r_mm*6+16, r_mm*2+5, 10)
-draw_dial(r_mm+5, r_mm+5, r_mm, shaft_r_mm, control=TRUE)
-draw_dial(3*r_mm+10, r_mm+5, r_mm, shaft_r_mm, control=TRUE)
-draw_dial(5*r_mm+15, r_mm+5, r_mm, shaft_r_mm, control=TRUE)
+draw_dial(r_mm+5, r_mm+5, r_mm, shaft_r_mm, T, control=TRUE)
+draw_dial(3*r_mm+10, r_mm+5, r_mm, shaft_r_mm, T, control=TRUE)
+draw_dial(5*r_mm+15, r_mm+5, r_mm, shaft_r_mm, T, control=TRUE)
 close.pdf()
 
 open.pdf("design_PDFs/small_parts.pdf", 120, 140, 10)
 for (i in 1:6)
-  draw_loop_lines_exact(7+i*8,12)
+  draw_small_support(7+i*6,12)
 
 for (i in 1:6)
   draw_L(i*7,21)
@@ -471,12 +576,12 @@ for (i in 1:6)
 draw_three_sheets(3,21)
 
 draw_three_separators(3, 50)
-draw_support(0, 72)
-draw_support(dial_opening_depth, 70)
-draw_support(dial_opening_depth*2, 70)
-draw_support(dial_opening_depth*2, 70-1.5*h_mm)
-draw_support(dial_opening_depth*2, 70-3*h_mm)
-draw_support(dial_opening_depth*2, 70+1.5*h_mm)
+draw_shaft_support(0, 72)
+draw_shaft_support(dial_opening_depth, 70)
+draw_shaft_support(dial_opening_depth*2, 70)
+draw_shaft_support(dial_opening_depth*2, 70-1.5*h_mm)
+draw_shaft_support(dial_opening_depth*2, 70-3*h_mm)
+draw_shaft_support(dial_opening_depth*2, 70+1.5*h_mm)
 
 draw_two_circles(52,40)
 
